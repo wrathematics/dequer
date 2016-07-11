f6d4c07f8ccba8594954223b2044ef788e902a16
Drew Schmidt <wrathematics@gmail.com>
2016-07-11 19:46:40 -0400

    fix peekers

--- R/peek.r
+++ R/peek.r
@@ -14,12 +14,18 @@ peekbacker <- function(x, n) headtail(x, n=1L, TAILS)
 
 
 
-#' peek
+#' peek/peekback
 #' 
-#' View items from the front of a deque or a stack. Similar to \code{head()}.
+#' View items from the front/back of a deque or a stack, for \code{peek()} or
+#' or \code{peekback()}, respectively. Similar to \code{head()} and \code{tail()}.
+#' 
+#' @description
+#' These methods are parallel safe.
 #' 
 #' @param x
 #' A deque or a stack.
+#' @param n
+#' The number of items to view.
 #' 
 #' @examples
 #' library(dequer)
@@ -28,45 +34,32 @@ peekbacker <- function(x, n) headtail(x, n=1L, TAILS)
 #' push(d, 2)
 #' peek(d)
 #' 
-#' @name peek
-#' @rdname peek
+#' @name peeking
+#' @rdname peeking
+NULL
+
+#' @rdname peeking
 #' @export
 peek <- function(x, n=1L) UseMethod("peek")
 
-#' @rdname peek
+#' @rdname peeking
 #' @export
 peek.deque <- peeker
 
-#' @rdname peek
+#' @rdname peeking
 #' @export
 peek.stack <- peeker
 
 
 
-#' peekback
-#' 
-#' View items from the back of a deque or a queue. Similar to \code{tail()}.
-#' 
-#' @param x
-#' A deque or a queue.
-#' 
-#' @examples
-#' library(dequer)
-#' d <- deque()
-#' push(d, 1)
-#' push(d, 2)
-#' peekback(d)
-#' 
-#' @name peekback
-#' @rdname peekback
+#' @rdname peeking
 #' @export
 peekback <- function(x, n=1L) UseMethod("peekback")
 
-
-#' @rdname peekback
+#' @rdname peeking
 #' @export
 peekback.deque <- peekbacker
 
-#' @rdname peekback
+#' @rdname peeking
 #' @export
 peekback.queue <- peekbacker
