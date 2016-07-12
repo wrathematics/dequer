fa4f783ee08d9cb7ea11c82b09b03c701cf97a87
Drew Schmidt <wrathematics@gmail.com>
2016-07-12 12:48:54 -0400

    fix printer

--- R/print.r
+++ R/print.r
@@ -1,32 +1,9 @@
-#' Printing Deques
-#' 
-#' @details
-#' If \code{output=="summary"}, then just a simple representation of the n-gram
-#' object will be printed.
-#' 
-#' If \code{output=="truncated"}, then the first 5 items will be
-#' printed.
-#' 
-#' If \code{output=="full"} then the full list will be printed.
-#' 
-#' @param x
-#' A deque.
-#' @param output
-#' A character string; determines what exactly is printed.
-#' Options are "summary", "truncated", and "full".
-#' @param ...
-#' Unused.
-#' 
-#' @name print-deque
-#' @rdname print-deque
-#' @method print deque
-#' @export
-print.deque <- function(x, ..., output="summary")
+printer <- function(x, ..., output="summary")
 {
   output <- match.arg(tolower(output), c("summary", "truncated", "full"))
   
   if (output == "summary")
-    print(paste("A deque object with", length(x), "elements."))
+    cat(paste("A", class(x), "with", length(x), "elements."), "\n")
   else
   {
     if (output == "truncated")
@@ -38,3 +15,42 @@ print.deque <- function(x, ..., output="summary")
   }
   invisible()
 }
+
+
+
+#' Printing Deques, Stacks, and Queues
+#' 
+#' @details
+#' If \code{output=="summary"}, then just a simple representation is printed.
+#' 
+#' If \code{output=="truncated"}, then the first 5 items will be
+#' printed.
+#' 
+#' If \code{output=="full"} then the full data structure will be printed.
+#' 
+#' @param x
+#' A deque.
+#' @param output
+#' A character string; determines what exactly is printed.
+#' Options are "summary", "truncated", and "full".
+#' @param ...
+#' Unused.
+#' 
+#' @name printer
+#' @rdname printer
+#' @method print deque
+#' @method print stack
+#' @method print queue
+NULL
+
+#' @rdname printer
+#' @export
+print.deque <- printer
+
+#' @rdname printer
+#' @export
+print.stack <- printer
+
+#' @rdname printer
+#' @export
+print.queue <- printer
